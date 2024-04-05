dec64722c40c5a34dbc8475bcd9beaabf1e7e868
Rem0o <remi.mercier.software@gmail.com>
2024-04-05 11:27:35 -0400

    Split test program and wrapper

---
+++ CtlLibraryCSharpWrapper/CtlLibraryHelpers.cs
@@ -0,0 +1,44 @@
+﻿using CtlLibraryBindings;
+using System;
+using System.Linq;
+
+namespace CtlLibraryCSharpWrapper
+{
+    public static class CtlLibraryHelpers
+    {
+        public static SWIGTYPE_p__ctl_device_adapter_handle_t[] GetDevices(SWIGTYPE_p__ctl_api_handle_t apiHandle)
+        {
+            using (var disposable = new CompositeDisposable())
+            {
+                var uintPtr = CtlLibrary.new_unsigned_int_Ptr().DisposeWith(disposable, CtlLibrary.delete_unsigned_int_Ptr);
+                CtlLibrary.unsigned_int_Ptr_assign(uintPtr, 0);
+                var emptyDeviceArrayPtr = CtlLibrary.new_ctl_device_adapter_handle_t_PtrPtr().DisposeWith(disposable, CtlLibrary.delete_ctl_device_adapter_handle_t_PtrPtr);
+
+                CtlLibrary.ctlEnumerateDevices(apiHandle, uintPtr, emptyDeviceArrayPtr).ThrowIfError("Enumerate devices (n)");
+                var n = Convert.ToInt32(CtlLibrary.unsigned_int_Ptr_value(uintPtr));
+
+                var array = new DeviceAdapterHandleArray(n).DisposeWith(disposable);
+                CtlLibrary.ctlEnumerateDevices(apiHandle, uintPtr, array.cast()).ThrowIfError($"Enumerate devices ({n})"); ;
+
+                var devices = Enumerable.Range(0, n).Select(array.getitem).ToArray();
+                return devices;
+            }
+        }
+        public static SWIGTYPE_p__ctl_fan_handle_t[] GetFanHandles(SWIGTYPE_p__ctl_device_adapter_handle_t device)
+        {
+            using (var disposable = new CompositeDisposable())
+            {
+                var uintPtr = CtlLibrary.new_unsigned_int_Ptr().DisposeWith(disposable, CtlLibrary.delete_unsigned_int_Ptr);
+
+                var emptyArrayPtr = CtlLibrary.new_ctl_fan_handle_t_PtrPtr().DisposeWith(disposable, CtlLibrary.delete_ctl_fan_handle_t_PtrPtr);
+                CtlLibrary.ctlEnumFans(device, uintPtr, emptyArrayPtr).ThrowIfError("Fan handles (n)");
+                int n = Convert.ToInt32(CtlLibrary.unsigned_int_Ptr_value(uintPtr));
+
+                var fanArray = new FanHandleArray(n).DisposeWith(disposable);
+                CtlLibrary.ctlEnumFans(device, uintPtr, fanArray.cast()).ThrowIfError($"Fan handles ({n})");
+
+                return Enumerable.Range(0, n).Select(fanArray.getitem).ToArray();
+            }
+        }
+    }
+}
--- CtlLibrarySample/Program.cs
+++ CtlLibrarySample/Program.cs
@@ -1,7 +1,6 @@
-﻿
-using CtlLibraryBindings;
+﻿using CtlLibraryBindings;
+using CtlLibraryCSharpWrapper;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,7 +13,7 @@ internal class Program
 
         using (disposable)
         {
-            var devices = GetDeviceHandles(disposable, apiHandle);
+            var devices = CtlLibraryHelpers.GetDevices(apiHandle);
             Console.WriteLine($"Found {devices.Length} cards");
             // SET SPEED
 
@@ -24,7 +23,7 @@ internal class Program
                 // version?
             }.DisposeWith(disposable);
 
-            var fanHandles = GetFanHandles(devices, disposable).ToArray();
+            var fanHandles = devices.SelectMany(d => CtlLibraryHelpers.GetFanHandles(d)).ToArray();
             Console.WriteLine($"Found {fanHandles.Length} fans");
 
             const int N = 10;
@@ -70,25 +69,6 @@ internal class Program
         }
 
 
-        static IEnumerable<SWIGTYPE_p__ctl_fan_handle_t> GetFanHandles(SWIGTYPE_p__ctl_device_adapter_handle_t[] devices, CompositeDisposable disposable)
-        {
-            var uintPtr = CtlLibrary.new_unsigned_int_Ptr().DisposeWith(disposable, CtlLibrary.delete_unsigned_int_Ptr);
-            foreach (var handle in devices)
-            {
-                var emptyArrayPtr = CtlLibrary.new_ctl_fan_handle_t_PtrPtr().DisposeWith(disposable, CtlLibrary.delete_ctl_fan_handle_t_PtrPtr);
-                CtlLibrary.ctlEnumFans(handle, uintPtr, emptyArrayPtr).ThrowIfError("Fan handles");
-                int n = Convert.ToInt32(CtlLibrary.unsigned_int_Ptr_value(uintPtr));
-
-                var fanArray = new FanHandleArray(n).DisposeWith(disposable);
-                CtlLibrary.ctlEnumFans(handle, uintPtr, fanArray.cast()).ThrowIfError("Fan handles");
-
-                foreach (var fan in Enumerable.Range(0, n).Select<int, SWIGTYPE_p__ctl_fan_handle_t>(fanArray.getitem))
-                {
-                    yield return fan;
-                }
-            }
-        }
-
         static SWIGTYPE_p__ctl_api_handle_t GetApiHandle(CompositeDisposable disposable)
         {
             ctl_init_args_t initArgs = CtlLibrary.create_Init_Args().DisposeWith(disposable);
@@ -98,22 +78,6 @@ internal class Program
             var apiHandle = CtlLibrary.ctl_api_handle_t_PtrPtr_value(handlePtr);
 
             return apiHandle;
-        }
-
-        static SWIGTYPE_p__ctl_device_adapter_handle_t[] GetDeviceHandles(CompositeDisposable disposable, SWIGTYPE_p__ctl_api_handle_t apiHandle)
-        {
-            var uintPtr = CtlLibrary.new_unsigned_int_Ptr().DisposeWith(disposable, CtlLibrary.delete_unsigned_int_Ptr);
-            CtlLibrary.unsigned_int_Ptr_assign(uintPtr, 0);
-            var emptyDeviceArrayPtr = CtlLibrary.new_ctl_device_adapter_handle_t_PtrPtr().DisposeWith(disposable, CtlLibrary.delete_ctl_device_adapter_handle_t_PtrPtr);
-
-            CtlLibrary.ctlEnumerateDevices(apiHandle, uintPtr, emptyDeviceArrayPtr).ThrowIfError("Enumerate devices (n)");
-            var n = Convert.ToInt32(CtlLibrary.unsigned_int_Ptr_value(uintPtr));
-
-            var array = new DeviceAdapterHandleArray(n).DisposeWith(disposable);
-            CtlLibrary.ctlEnumerateDevices(apiHandle, uintPtr, array.cast()).ThrowIfError($"Enumerate devices ({n})"); ;
-
-            var devices = Enumerable.Range(0, n).Select(array.getitem).ToArray();
-            return devices;
         }
 
         static void GetSpeed(CompositeDisposable disposable, SWIGTYPE_p__ctl_fan_handle_t fan)
