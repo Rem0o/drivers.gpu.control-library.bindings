b09b748c134835673a5f4b9a4fc89807449a8735
Rem0o <remi.mercier.software@gmail.com>
2024-07-12 15:48:06 -0400

    Update CtlLibraryHelper.cpp

--- CtlLibraryCSharpBinds/CtlLibraryHelper.cpp
+++ CtlLibraryCSharpBinds/CtlLibraryHelper.cpp
@@ -3,9 +3,9 @@
 ctl_result_t CTL_APICALL CtlLibraryHelper::SetFlatFanSpeedTable(ctl_fan_handle_t fanHandle, ctl_fan_speed_table_t* table, int speedPercent)
 {
 	// setup the table if not already done
-	if (table->Size == 0)
+	if (table->numPoints != tableNumPoints)
 	{
-		table->Size = sizeof(ctl_fan_speed_table_t);
+		table->numPoints = tableNumPoints;
 
 		ctl_fan_temp_speed_t* pair;
 		for (int i = 0; i < table->numPoints; i++)
@@ -19,7 +19,6 @@ ctl_result_t CTL_APICALL CtlLibraryHelper::SetFlatFanSpeedTable(ctl_fan_handle_t
 	}
 
 	// apply
-	table->numPoints = tableNumPoints;
 	for (int i = 0; i < table->numPoints; i++)
 	{
 		table->table[i].speed.speed = speedPercent;
