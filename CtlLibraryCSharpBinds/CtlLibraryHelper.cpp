f04c6c59e8db72ecb8b21801ab2e790893902186
Rem0o <remi.mercier.software@gmail.com>
2024-10-15 09:16:01 -0400

    6 points

--- CtlLibraryCSharpBinds/CtlLibraryHelper.cpp
+++ CtlLibraryCSharpBinds/CtlLibraryHelper.cpp
@@ -13,7 +13,7 @@ ctl_result_t CTL_APICALL CtlLibraryHelper::SetFlatFanSpeedTable(ctl_fan_handle_t
 			table->table[i] = { 0 };
 			table->table[i].Version = 0;
 			table->table[i].Size = sizeof(ctl_fan_temp_speed_t);
-			table->table[i].temperature = ((i + 1) * 20);
+			table->table[i].temperature = 25 + ((i + 1) * 12);
 			table->table[i].speed.Version = 0;
 			table->table[i].speed.units = CTL_FAN_SPEED_UNITS_PERCENT;
 			table->table[i].speed.Size = sizeof(ctl_fan_speed_t);
--- CtlLibraryCSharpBinds/CtlLibraryHelper.h
+++ CtlLibraryCSharpBinds/CtlLibraryHelper.h
@@ -3,7 +3,7 @@
 
 namespace CtlLibraryHelper {
 
-	const int tableNumPoints = 5;
+	const int tableNumPoints = 6;
 
 	ctl_result_t CTL_APICALL SetFlatFanSpeedTable(ctl_fan_handle_t fanHandle, ctl_fan_speed_table_t* table, int speedPercent);
 }
